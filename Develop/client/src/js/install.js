01e315e0d305b94fb4bffac24d53fcc7bf94259f
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-09-08 12:46:32 -0400

    butisstall event listerner

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -9,7 +9,21 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (deferredPrompt) {
+        // Show the browser's install prompt
+        deferredPrompt.prompt();
+        const choiceResult = await deferredPrompt.userChoice;
+        
+        if (choiceResult.outcome === 'accepted') {
+          console.log('PWA install Accepted!!');
+        } else {
+          console.log('PWA install cancelled!!');
+        }
+        deferredPrompt = null;
+        butInstall.style.display = 'none';
+      }
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
