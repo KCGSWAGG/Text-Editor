2f25de674d12c1575468c0a9d4b73e56cfa33f89
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-09-08 13:00:27 -0400

    injecctmanifest

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,15 +3,12 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -30,11 +27,33 @@ module.exports = () => {
         theme_color: '#000000',
         icons: [
           {
+            src: path.resolve('src/images/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src/service-worker.js', // Your service worker source file
+        swDest: 'service-worker.js', // Destination file in the output directory
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
       ],
     },
   };
